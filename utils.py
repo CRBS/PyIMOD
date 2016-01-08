789ef38bcedd754311c42cb84e5f0ddbd278cabd
Alex Perez <alexjperez@gmail.com>
2016-01-08 09:27:03 -0800

    Added function set_bit to modifty bits of a 32-bit integer.

--- utils.py
+++ utils.py
@@ -9,3 +9,14 @@ def is_string(var, varStr):
         raise ValueError('{0} must be a string.'.format(varStr))
     else:
         return
+
+def set_bit(v, index, x):
+    """
+    http://stackoverflow.com/questions/12173774/modify-bits-in-an-integer-
+    in-python
+    """
+    mask = 1 << index
+    v &= ~mask
+    if x:
+        v |= mask
+    return v
