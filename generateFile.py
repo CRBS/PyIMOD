2bc5579b2445c550dfdfb6bd0888f7a8680c5ce7
Alex Perez <alexjperez@gmail.com>
2016-01-08 11:05:22 -0800

    Initial commit.

--- generateFile.py
+++ generateFile.py
@@ -1,7 +1,8 @@
 from .ImodModel import ImodModel
 from .ImodObject import ImodObject
+from .ImodWrite import ImodWrite
 
-def blank_training_file():
+def blankTrainingModel(filename):
     # Create ImodModel class and two ImodObject sub-classes
     model = ImodModel()
     model.Objects.append(ImodObject())
@@ -12,9 +13,15 @@ def blank_training_file():
     model.Objects[0].setName('Seed Points')
     model.Objects[0].setColor('0,1,0')
     model.Objects[0].setObjectType('scattered')
+    model.Objects[0].setSymbolType('circle')
+    model.Objects[0].setSymbolSize(10)
+    model.Objects[0].setSymbolFillOn()
 
     # Set object properties of Object 2 (training contours)
     model.Objects[1].setName('Training Contours')
     model.Objects[1].setColor('0,1,1')
     model.Objects[1].setObjectType('closed')
     model.Objects[1].setLineWidth(2)
+
+    # Write file
+    ImodWrite(model, filename)
