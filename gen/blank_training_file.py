f0c78212bc18c3dbcfe1b85861c9a82dc3fe2a48
Alex Perez <alexjperez@gmail.com>
2016-01-08 09:27:29 -0800

    Added object names.

--- gen/blank_training_file.py
+++ gen/blank_training_file.py
@@ -7,8 +7,10 @@ def blank_training_file():
     model.Objects.append(ImodObject())
     model.nObjects = 2
 
+    model.Objects[0].setName('Seed Points')
     model.Objects[0].setColor('0,1,0')
 
-    model.Objects[1].setLineWidth(2)
+    model.Objects[1].setName('Training Contours')
     model.Objects[1].setColor('0,1,1')
+    model.Objects[1].setLineWidth(2)
 
