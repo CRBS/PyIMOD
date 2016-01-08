dadb16ce683bd8f6e882c309f98af5ca93e51476
Alex Perez <alexjperez@gmail.com>
2016-01-08 11:05:02 -0800

    Supports import of generateFile.

--- __init__.py
+++ __init__.py
@@ -2,8 +2,4 @@ from ImodModel import ImodModel
 from ImodObject import ImodObject
 from ImodContour import ImodContour
 from ImodWrite import ImodWrite
-
-from pyimod import ImodModel
-from pyimod import ImodObject
-from pyimod import ImodContour
-from pyimod import ImodWrite
+from generateFile import *
