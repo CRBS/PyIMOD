87235df2c0f9494bd8e0eebc8ee6f3cac6b0adff
Alex Perez <alexjperez@gmail.com>
2016-01-20 13:21:29 -0800

    Added tutorialModel to generated a tutorial model file.

--- ImodGen.py
+++ ImodGen.py
@@ -3,7 +3,7 @@ from .ImodObject import ImodObject
 from .ImodWrite import ImodWrite
 from .utils import ImodCmd
 
-def blankTrainingModel(filename):
+def blankTrainingModel(filename = None):
     """
     Creates a blank model file to be used for generating CHM training images
     and labels using IMOD. The output model file can be loaded on any MRC
@@ -28,5 +28,51 @@ def blankTrainingModel(filename):
     model.Objects[1].setObjectType('closed')
     model.Objects[1].setLineWidth(2)
 
-    # Write file
-    ImodWrite(model, filename)
+    # Write or return file
+    if filename:
+        ImodWrite(model, filename)
+    else:
+        return model
+
+def tutorialModel(filename = None):
+    """
+    Creates a model file used in pyimod tutorials. Model file containts 25
+    objects consisting of spheres and cubes of various sizes and positions.
+    """
+    model = ImodModel()
+    model.setImageSize(1000, 1000, 1000)
+    model.setPixelSizeXY(4)
+    model.setPixelSizeZ(4)
+    model.setUnits('nm')
+
+    # Make big central sphere
+    model.genSphereObject([500, 500, 500], 200, 100)
+
+    # Make small, radius 50 spheres at 10 defined locations
+    centers = [[700, 700, 700], [700, 800, 400], [300, 300, 500], 
+        [450, 300, 100], [100, 100, 100], [850, 200, 600], [400, 700, 300],
+        [800, 100, 700], [200, 900, 400]]
+    [model.genSphereObject(x, 50, 25) for x in centers]
+
+    # Make medium, radius 100 spheres at 10 defined locations
+    centers = [[200, 200, 400], [150, 750, 800], [900, 500, 500],
+        [800, 350, 350], [500, 100, 100]]
+    [model.genSphereObject(x, 100, 50) for x in centers]
+
+    # Make small, width 50 cubes at 5 defined locations
+    centers = [[150, 500, 500], [850, 900, 850], [700, 200, 200],
+        [850, 150, 800], [500, 500, 800]]
+    [model.genCubeObject(x, 50) for x in centers]
+
+    # Make medium, width 100 cubes at 5 defined locations
+    centers = [[450, 850, 700], [800, 500, 850], [150, 500, 250],
+        [750, 800, 200], [250, 200, 800]]
+    [model.genCubeObject(x, 100) for x in centers]
+
+    # Mesh all objects, with capping turned on
+    model = ImodCmd(model, 'imodmesh -C')
+  
+    if filename:
+        ImodWrite(model, filename)
+    else: 
+        return model 
