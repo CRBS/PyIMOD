199b70ed9393560eabf924f01ced163c6ef669f1
Alex Perez <alexjperez@gmail.com>
2016-01-20 11:24:34 -0800

    Added ImodCmd function to run IMOD command-line programs.

--- utils.py
+++ utils.py
@@ -1,3 +1,19 @@
+def ImodCmd(imodModel, cmdStr):
+    from .ImodModel import ImodModel
+    from .ImodWrite import ImodWrite
+    from subprocess import call
+    import os
+    import random
+    import string
+    fname = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(30))
+    ImodWrite(imodModel, fname)
+    cmd = cmdStr + ' ' + fname + ' ' + fname
+    call(cmd.split())
+    imodModel = ImodModel(fname)
+    os.remove(fname)
+    os.remove(fname + '~')
+    return imodModel
+
 def is_integer(var, varStr):
     if not isinstance(var, (int, long)):
         raise ValueError('{0} must be an integer.'.format(varStr))
