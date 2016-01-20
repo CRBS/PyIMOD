f1c3680d4c03372e4338b5fa18629c2444bced65
Alex Perez <alexjperez@gmail.com>
2016-01-20 11:24:14 -0800

    Supports ImodCmd from utils.

--- __init__.py
+++ __init__.py
@@ -2,4 +2,4 @@ from ImodModel import ImodModel
 from ImodObject import ImodObject
 from ImodContour import ImodContour
 from ImodWrite import ImodWrite
-from ImodGen import *
+from utils import ImodCmd
