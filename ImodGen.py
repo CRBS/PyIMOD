2a7a57253d50ce5b1a910e7104e280c09de06544
Alex Perez <alexjperez@gmail.com>
2016-01-20 13:03:20 -0800

    Corrected some issues with generating a blank training model file.

--- ImodGen.py
+++ ImodGen.py
@@ -1,6 +1,7 @@
 from .ImodModel import ImodModel
 from .ImodObject import ImodObject
 from .ImodWrite import ImodWrite
+from .utils import ImodCmd
 
 def blankTrainingModel(filename):
     """
@@ -10,13 +11,12 @@ def blankTrainingModel(filename):
     """
     # Create ImodModel class and two ImodObject sub-classes
     model = ImodModel()
-    model.Objects.append(ImodObject())
-    model.Objects.append(ImodObject())
-    model.nObjects = 2
+    model.addObject()
+    model.addObject()
 
     # Set object properties of Object 1 (seed points)
     model.Objects[0].setName('Seed Points')
-    model.Objects[0].setColor('0,1,0')
+    model.Objects[0].setColor(0, 1, 0)
     model.Objects[0].setObjectType('scattered')
     model.Objects[0].setSymbolType('circle')
     model.Objects[0].setSymbolSize(10)
@@ -24,7 +24,7 @@ def blankTrainingModel(filename):
 
     # Set object properties of Object 2 (training contours)
     model.Objects[1].setName('Training Contours')
-    model.Objects[1].setColor('0,1,1')
+    model.Objects[1].setColor(0, 1, 1)
     model.Objects[1].setObjectType('closed')
     model.Objects[1].setLineWidth(2)
 
