934e6b5c1b8f7da832b86e762eb6b5fe773d5676
Alex Perez <alexjperez@gmail.com>
2016-01-22 17:14:55 -0800

    Added steps to remove small contours and empty objects.

--- scripts/proc_full_cell.py
+++ scripts/proc_full_cell.py
@@ -5,8 +5,21 @@ from pyimod import *
 
 file = sys.argv[1]
 
+# Load model
 mod = ImodModel(file)
+
+# Remove empty objects (i.e. objects with zero contours)
+mod.filterByNContours('>', 0)
+
+# Remove all small contours, as these can cause problems with meshing and
+# quantification in Amira.
+mod.removeSmallContours()
+
+# Set properties from the neuron_scn table
 mod.setFromTable('neuron_scn')
+
+# Mesh all objects
 mod = ImodCmd(mod, 'imodmesh -CTs -P 4')
 
+# Write file
 ImodWrite(mod, file + '_out')
